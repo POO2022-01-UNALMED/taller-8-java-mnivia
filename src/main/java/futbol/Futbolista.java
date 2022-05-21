540e04bc77f876ff06c1f9cb8064b270c00edbca
Marlon <mnivia@unal.edu.co>
2022-05-21 10:47:48 -0500

    commit en src

---
+++ src/main/java/futbol/Futbolista.java
@@ -0,0 +1,59 @@
+package futbol;
+
+public abstract class Futbolista implements Comparable<Futbolista> {
+
+	private String nombre;
+	private int edad;
+	private final String posicion; // No debe cambiar una vez establecida
+
+	public Futbolista() {
+		this("Maradona",30,"delantero");
+	}
+
+	public Futbolista(String nombre, int edad, String posicion) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.posicion = posicion;
+	}
+
+	@Override
+	public String toString() {
+		return "El futbolista " + nombre + " tiene " + edad + ", y juega de " + posicion ;
+	}
+
+	public boolean equals(Futbolista f) {
+		if(this.nombre.equals(f.nombre) && this.edad==f.edad && this.posicion.equals(f.posicion)) {
+			return true;
+		}
+		return false;
+	}
+
+
+
+	public abstract boolean jugarConLasManos();
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getPosicion() {
+		return posicion;
+	}
+
+
+}
---
+++ src/main/java/futbol/Jugador.java
@@ -0,0 +1,43 @@
+package futbol;
+
+public class Jugador extends Futbolista{
+
+	public short golesMarcados;
+	public byte dorsal;
+
+
+
+	public Jugador() {
+		super();
+		this.golesMarcados=289;
+		this.dorsal=7;
+		// TODO Auto-generated constructor stub
+	}
+
+	public Jugador(String nombre, int edad, String posicion,short golesMarcados, byte dorsal) {
+		super(nombre, edad, posicion);
+		this.golesMarcados=golesMarcados;
+		this.dorsal=dorsal;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int compareTo(Futbolista o) {
+		// TODO Auto-generated method stub
+		return (Math.abs(this.getEdad()-o.getEdad()));
+	}
+
+	@Override
+	public boolean jugarConLasManos() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "El futbolista " + this.getNombre() + " tiene" + this.getEdad()  + "y juega de "+ this.getPosicion() + " con el dorsal "+ this.dorsal+ ".Ha marcado "+ golesMarcados;
+	}
+
+
+
+}
